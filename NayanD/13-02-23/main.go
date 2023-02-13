6562896aca3e723636477743d7b31d5c30688315
Nayandhoble <103194199+Nayandhoble@users.noreply.github.com>
2023-02-13 17:08:53 +0530

    Add files via upload

---
+++ NayanD/13-02-23/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+var card string = "nayan"
+
+//card = "patya"
+//card := "nayan"
+func main() {
+
+	card = "patya"
+
+	fmt.Println(card)
+}
---
+++ NayanD/13-02-23/main1.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	//var card string = "ACE OF SPADES"
+	card := "ACE OF SPADES"
+
+	//Assigning new value to variable card
+	card = "Five of diamonds"
+
+	fmt.Println(card)
+}
