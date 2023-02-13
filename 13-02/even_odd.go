13f75d15d46463a0d8aea4bc3d0af44585c37fbc
prathmeshdalve <prathmeshdalve.dbdamarch22@gmail.com>
2023-02-13 17:02:14 +0530

    13-02

---
+++ 13-02/even_odd.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("Enter number : ")
+	var n int
+	fmt.Scanln(&n)
+	if n%2 == 0 {
+		fmt.Println(n, "is Even number")
+	} else {
+		fmt.Println(n, "is Odd number")
+	}
+}
