a24a7190da16ab0d6f951bc0308eaf04cf3edded
dhirajdeore619 <103194286+dhirajdeore619@users.noreply.github.com>
2023-02-13 12:10:07 +0530

    Create hello.go

---
+++ dhiraj_deore/13-02-23/hello.go
@@ -0,0 +1,10 @@
+// code for print something golang
+
+package main //package declaration
+
+import "fmt" //import other packages that we need
+
+// declare function by using func keyword
+func main() {
+	fmt.Println("hello world !!")
+}
