3088cfe417f33666775a94244f4b72ee26dec5cb
Malika Sikri <malika@infobellit.com>
2023-02-21 16:25:08 +0530

    Assignment 2 - Constant & Variable

---
+++ Malika/Day 2/Variables.go
@@ -0,0 +1,23 @@
+package main
+
+//This section tells the go compiler that the
+//package should compile as an executable program
+
+import (
+	"fmt"
+) //This section tells that the "fmt = format" package is being used in the program
+
+var x int = 20
+var y bool = true
+var z string = "Learning Golang"
+var ab float64 = 776.87
+
+// Declaration and assignment of the variables
+
+func main() {
+	fmt.Println(x)  //Printing the assigned value for the variable "x"
+	fmt.Println(y)  //Printing the assigned value for the variable "y"
+	fmt.Println(z)  //Printing the assigned value for the variable "z"
+	fmt.Println(ab) //Printing the assigned value for the variable "ab"
+
+}
