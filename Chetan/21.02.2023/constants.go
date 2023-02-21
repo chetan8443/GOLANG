51520fdb9a434e1cbf8938168853bce29f92a38a
CHETAN PATIL <113095937+chetan8443@users.noreply.github.com>
2023-02-21 15:18:11 +0530

    assignment on constatnts variables

---
+++ Chetan/21.02.2023/constants.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+
+func main(){
+const a bool=true           //declaring & intitializing string variable
+const b int=47            //declaring & intitializing string variable
+const c string="CHETAN" //declaring & intitializing string variable
+const d float64=3.14    //declaring & intitializing string variable
+  
+  //printing all the constants ,After initializing constatnt variable we can't reinitialize them
+fmt.Printf("Boolean : %v \n integer: %d \n String : %s\n Float64 : %f", a,b,c,d)
+
+
+
+}
