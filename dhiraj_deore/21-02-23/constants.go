80b42e60952427be0c3e0a111a3d2dbb7412d31f
dhirajdeore619 <103194286+dhirajdeore619@users.noreply.github.com>
2023-02-21 15:30:28 +0530

    Create constants.go

---
+++ dhiraj_deore/21-02-23/constants.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+const a = 10         // global constant of int datatype
+const b = "Infobell" // global constant of string type
+
+func main() {
+	const a = 20.12 // local constant of float64 type
+	const b = false // constant of
+
+	fmt.Println("value of local variable a and b :", a, b) // printing valus of local connstants
+	fmt.Printf("type of local variable a and b :%T, %T \n", a, b)
+	printGlobal()
+
+}
+
+func printGlobal() { // function will print values and type of global constants
+	fmt.Println("global value of variable a and b :", a, b)
+	fmt.Printf("type of global variable a and b :%T, %T \n", a, b)
+}
