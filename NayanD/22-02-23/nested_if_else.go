c11a0baf506c4ea20b913dc9801a9ae19186df73
Nayandhoble <103194199+Nayandhoble@users.noreply.github.com>
2023-02-22 17:34:24 +0530

    Update nested_if_else.go

--- NayanD/22-02-23/nested_if_else.go
+++ NayanD/22-02-23/nested_if_else.go
@@ -1,3 +1,5 @@
+// Write a program based on the Nested if condition.
+
 package main
 
 import "fmt"
@@ -7,7 +9,7 @@ func main() {
 	var balance int
 
 	fmt.Println("Enter your balance: ")
-	fmt.Scanf("%d", &balance)
+	fmt.Scanf("%d", &balance)               // taking input from user 
 
 	if balance <= 0 {
 		fmt.Printf("Your balance is low ,Please add some funds or penalty will be charged ")
