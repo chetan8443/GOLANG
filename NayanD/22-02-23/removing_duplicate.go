bf665236ffcd7d82ed39ffc2da342c90a11552e1
Nayandhoble <103194199+Nayandhoble@users.noreply.github.com>
2023-02-22 17:32:59 +0530

    Update removing_duplicate.go

--- NayanD/22-02-23/removing_duplicate.go
+++ NayanD/22-02-23/removing_duplicate.go
@@ -1,13 +1,15 @@
+//write a program to remove duplicate from slice
+
 package main
 
 import "fmt"
 
 func main() {
-	list := []int{1, 2, 4, 3, 2, 7, 5, 4, 5, 6, 7, 8, 6}
+	list := []int{1, 2, 4, 3, 2, 7, 5, 4, 5, 6, 7, 8, 6}  // slice containing duplicate values
 
-	new_list := []int{}
+	new_list := []int{}       // empty slice 
 
-	for i := 0; i < len(list); i++ {
+	for i := 0; i < len(list); i++ {        // iterating over a slice 
 		count := 1
 
 		for j := 0; j < len(new_list); j++ {
@@ -17,9 +19,9 @@ func main() {
 
 		}
 		if count == 1 {
-			new_list = append(new_list, list[i])
+			new_list = append(new_list, list[i])     // appending unique values to empty slice
 
 		}
 	}
-	fmt.Println(new_list)
+	fmt.Println(new_list)           // printing unique slice elements
 }
