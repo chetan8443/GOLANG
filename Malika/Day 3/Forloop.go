74f4ed44b6881ec48768a0a734853f167c32b417
Malika Sikri <malika@infobellit.com>
2023-02-23 00:02:56 +0530

    Day 3

---
+++ Malika/Day 3/Forloop.go
@@ -0,0 +1,38 @@
+/*
+Write a program that based on For Loop statements.
+*/
+
+package main
+
+import "fmt"
+
+var v int = 200
+
+func main() {
+	 
+	//using if statement in for loop to check the condition
+
+	if v < 0 {
+		fmt.printf("v is less than 0\n")
+		if i == 0 {
+			break
+		}
+	}
+
+	else
+
+	{ for i := v,i < 200; i += 10 
+	fmt.Println(i)
+}
+
+/*
+func main() {
+	a := 5
+	for i := a; i < 100; i += 5 { // for loop with break statement used
+		if i == 50 {
+			break
+		}
+		fmt.Println(i)
+	}
+}
+*/
