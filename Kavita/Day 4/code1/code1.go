6e1d8084e3a67fa303a7bad61997da8ee8630852
DESKTOP-JD1I5QH\Kavita <kavita@infobellit.com>
2023-02-23 16:12:55 +0530

    code to call functioin from diffrent file in the same folder

---
+++ Kavita/Day 4/code1/code1.go
@@ -0,0 +1,6 @@
+package main
+
+func main() {
+	PrintLine("code2")
+
+}
---
+++ Kavita/Day 4/code1/code2.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func PrintLine(str string) {
+	fmt.Println("this is from a diffrent function file in the same folder", str)
+}
