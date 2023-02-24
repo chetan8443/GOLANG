e42aec100696e546fb5f51c3fe74a817d3e30611
vsankar90 <119859561+vsankar90@users.noreply.github.com>
2023-02-24 14:21:34 +0530

    Create main.go

---
+++ Sankar/Day4/main.go
@@ -0,0 +1,19 @@
+package main 
+import (
+	"fmt"
+	a "go-lang/test"
+	b "go-lang/test"
+	c "go-lang/test/unittest"
+)
+
+func suum(x int, y int)int{
+	summer:=x+y
+	return summer 
+}
+func main(){
+	a.Multi(10,5)
+	b.Dispy(122222)
+	c.Welcomebhai()
+	fmt.Println(suum(10,20))
+	
+}
