cbf0defe283634574e84809fb455eb3820ed0319
CHETAN PATIL <113095937+chetan8443@users.noreply.github.com>
2023-02-24 15:22:19 +0530

    Add files via upload

---
+++ Chetan/24.02.2023/pattern2.go
@@ -0,0 +1,43 @@
+
+package main
+
+import "fmt"
+
+func main() {
+	var count int = 6
+
+	for j := 1; j < count; j++ {
+
+		for i := count; i >j; i-- {
+			fmt.Print(" ")
+		}
+		for k := 0; k < j; k++ {
+			fmt.Print("* ")
+		}
+		fmt.Println("")
+	}
+	for j := 1; j < count; j++ {
+
+		
+		for k := 0; k < j; k++ {
+			fmt.Print(" ")
+		}
+		for i := count; i >j; i-- {
+			fmt.Print("* ")
+		}
+		fmt.Println("")
+	}
+
+}
+
+
+//      * 
+//     * *
+//    * * *
+//   * * * *
+//  * * * * *
+//  * * * * *
+//   * * * *
+//    * * *
+//     * *
+//      *
