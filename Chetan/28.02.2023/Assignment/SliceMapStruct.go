96f082292b2a6e50e90c05f4df93eb6ca527a284
CHETAN PATIL <113095937+chetan8443@users.noreply.github.com>
2023-02-28 15:40:18 +0530

    Update SliceMapStruct.go

--- Chetan/28.02.2023/Assignment/SliceMapStruct.go
+++ Chetan/28.02.2023/Assignment/SliceMapStruct.go
@@ -1,6 +1,7 @@
 package main
 
 import "fmt"
+// created slice and map using struct
 
 func main() {
 	type s1 struct {
@@ -10,8 +11,12 @@ func main() {
 		roll_no int
 		name    string
 	}
+	
+		//created slice by using struct 's2'
+
 	s := []s2{{1, "arbind"}, {2, "yogesh"}}
 
+	//created map by using struct 's2'
 	m := map[int]s1{1: {"orange"}, 2: {"red"}, 3: {"yellow"}}
 	
 	for k,v:=range m{
