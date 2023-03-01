cca55fca255d72162ce4c7604dfc96e334de543b
Prathmesh Dalve <102685950+PrathmeshDalve@users.noreply.github.com>
2023-03-01 14:09:47 +0530

    Day7Assignment & Programs

---
+++ Prathmesh/28-02/structs1.go
@@ -0,0 +1,23 @@
+// 1.Program to create an embedded struct
+
+package main
+
+import "fmt"
+
+type person struct { // create a struct
+	fname string
+	lname string
+}
+
+type student struct { //create a embedded struct
+	rollno int
+	person // using struct as a field
+}
+
+func main() {
+
+	s1 := student{1, person{"Alex", "carey"}}
+
+	fmt.Printf("%+v", s1)
+	fmt.Println(s1)
+}
