c9d1280333bf559a5a0ff4f8308c7d1e91088537
Prathmesh Dalve <102685950+PrathmeshDalve@users.noreply.github.com>
2023-03-02 15:42:17 +0530

    Day8Assignment & Programs

--- Prathmesh/01-03/swap_pointers.go
+++ Prathmesh/01-03/swap_pointers.go
@@ -1,20 +1,45 @@
+/*
 package main
 
 import "fmt"
 
-func swap(px, py *int) {
-	tempx := *px
-	tempy := *py
-	*px = tempy
-	*py = tempx
-}
-func main() {
-	x := int(1)
-	y := int(2)
-	fmt.Println("x was", x)
-	fmt.Println("y was", y)
-	swap(&x, &y)
+	func swap(px, py *int) {
+		tempx := *px
+		tempy := *py
+		*px = tempy
+		*py = tempx
+	}
 
-	fmt.Println("x is now", x)
-	fmt.Println("y is now", y)
+	func main() {
+		x := int(1)
+		y := int(2)
+		fmt.Println("x was", x)
+		fmt.Println("y was", y)
+		swap(&x, &y)
+
+		fmt.Println("x is now", x)
+		fmt.Println("y is now", y)
+	}
+*/
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type Message struct {
+	Text string `json:"text"`
+}
+
+func main() {
+	http.HandleFunc("/", helloHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	message := Message{Text: "Hello, World!"}
+	json.NewEncoder(w).Encode(message)
 }
