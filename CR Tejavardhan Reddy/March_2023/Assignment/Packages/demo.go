5288e3c478585839032e96943c91edb014ff702d
tejavard <crtejavardhanreddy123@gmail.com>
2023-03-08 22:54:03 +0530

    assignment

--- CR Tejavardhan Reddy/March_2023/Assignment/Packages/demo.go
+++ CR Tejavardhan Reddy/March_2023/Assignment/Packages/demo.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
+
+var teja struct {
+	inte int
+}
 
 func main() {
+	var slice = make([]int, 0)
 	fmt.Println("Golang packages")
+	str_marshal, _ := json.Marshal("String")
+	float_marshal, _ := json.Marshal(3.14)
+	struct_marshal, _ := json.Marshal(teja)
+	fmt.Println("After marshalling", str_marshal, float_marshal, struct_marshal, slice)
+	// var struct_unmarshal teja
+	// ba:=[]byte(teja.inte)
+	// json.Unmarshal(ba, &struct_unmarshal)
+	json.Unmarshal()
 }
