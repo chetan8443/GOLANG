51ff54273db62707621af14ce39540cd257aea2e
meer8586 <100994911+meer8586@users.noreply.github.com>
2023-03-08 17:38:39 +0530

    8th March

---
+++ Meer/08-03-2023/DB_Connection/sample.sql
@@ -0,0 +1,2 @@
+Create table if not exists student (firstName varchar(20), lastName varchar(20), emailId varchar(20));
+INSERT INTO student (firstName, lastName, emailId) VALUES ('Meer','Syed','fd@gmail.com');
