913a97d8d19dd6f20fde6d96cc81e33b7274bc9c
Vishal Pimple <104988734+VishStrong@users.noreply.github.com>
2023-03-08 09:58:22 +0530

    Update test.go

--- Vishal/Day12/test.go
+++ Vishal/Day12/test.go
@@ -1,1 +1,32 @@
 
+package main
+//Progam to create a text file and write content in it
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	fmt.Println("execution started")
+	var names =[]string{"vipul ","Yogesh ","Vivek ","Vishal "}
+   file,err:=os.OpenFile("details.txt",os.O_WRONLY|os.O_CREATE,0644)// creating file and giving permissions to it
+   defer file.Close()
+   if err!=nil {
+	log.Fatal(err)
+   }
+   bufferWriter:=bufio.NewWriter(file)    //using bufferwriter to write data in that file as string
+	for _, v := range names {
+		
+		wr,err:=bufferWriter.WriteString(v)
+		// 
+		_=wr
+		if err != nil {
+			log.Fatal(err)
+		}
+		bufferWriter.Flush()    
+
+	}
+	
+}
