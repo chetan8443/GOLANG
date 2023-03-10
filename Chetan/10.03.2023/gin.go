f3bfd28bc85a94f6ba0335c7340f6426d19facf1
CHETAN PATIL <113095937+chetan8443@users.noreply.github.com>
2023-03-10 18:10:38 +0530

    Create gin.go

---
+++ Chetan/10.03.2023/gin.go
@@ -0,0 +1,105 @@
+// Create a program with a GET API that retrieves data by Id.
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Student struct {
+	Name    string `json:"Name"`
+	Id      int    `json:"Id"`
+	SurName string `json:"SurName"`
+}
+
+var student []Student
+
+func main() {
+	student = []Student{
+		{Name: "Shreya", Id: 1, SurName: "patil"},
+		{Name: "swara", Id: 2, SurName: "patil"},
+		{Name: "Jayanta", Id: 3, SurName: "patil"}}
+	r := gin.Default()
+
+	r.GET("/getStudent/:id", getStudentById)
+	r.GET("/getStudent", getStudents)
+	r.DELETE("/delete/:id", dStudentById)
+	r.PUT("/update", UpdateStudent)
+
+	r.Run(":8080")
+
+}
+
+func getStudents(c *gin.Context) {
+
+	c.IndentedJSON(http.StatusOK, student)
+
+}
+
+// func getStudent(w http.ResponseWriter, r *http.Request){
+
+// }
+
+func getStudentById(c *gin.Context) {
+
+	id, _ := strconv.Atoi(c.Param("id"))
+
+	for i, s := range student {
+		if id == s.Id {
+			c.IndentedJSON(http.StatusOK, student[i])
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "student not found"})
+}
+
+func dStudentById(c *gin.Context) {
+
+	id, _ := strconv.Atoi(c.Param("id"))
+
+	for i, s := range student {
+		if id == s.Id {
+			student = append(student[:i], student[i+1:]...)
+
+			c.JSON(http.StatusOK, gin.H{"message": "student details have been Removed successfully"})
+
+			return
+		}
+	}
+
+}
+
+func UpdateStudent(c *gin.Context) {
+
+	var std Student
+
+	id, _ := strconv.Atoi(c.Param("id"))
+	db, _ := ioutil.ReadAll(c.Request.Body)
+
+	c.JSON(http.StatusOK, string(db))
+
+	err := c.ShouldBind(&std)
+
+	c.JSON(http.StatusOK, std)
+
+	if err != nil {
+		http.Error(c.Writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	for i, s := range student {
+		if id == s.Id {
+			student = append(student[:i], student[i+1:]...)
+			student = append(student, std)
+
+			c.JSON(http.StatusOK, gin.H{"message": "Student details have been Updated successfully"})
+
+			return
+		}
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Student details not Found"})
+
+}
