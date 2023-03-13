c96d7b9f8b2249cbbd5b5401f4ab205d911167d0
CHETAN PATIL <113095937+chetan8443@users.noreply.github.com>
2023-03-13 16:59:58 +0530

    assignment upload

--- Chetan/13.03.2023/Assignment/main.go
+++ Chetan/13.03.2023/Assignment/main.go
@@ -1,7 +1,7 @@
-package main
-
 // 1)how can you find the date that is exactly one year and two days after the current date?
 // 2) Write a program that prints the current time in two different time zones.
+
+package main
 
 import (
 	"fmt"
@@ -9,32 +9,32 @@ import (
 )
 
 func main() {
-	timeXone()
-	additon()
+	timeXone() //Calling function for current time of different zones
+	additon()  //Calling function for the date that is exactly one year and two days after the current date
 }
 
 func timeXone() {
 
-	loc, _ := time.LoadLocation("Asia/Shanghai")
+	loc, _ := time.LoadLocation("Asia/Shanghai") //LoadLocation returns the Location with the given name
 	loc1, _ := time.LoadLocation("Asia/Kolkata")
-	//set timezone,
-	now := time.Now().In(loc)
+	
+	now := time.Now().In(loc) //It returns current time of given location
 	now1 := time.Now().In(loc1)
-	fmt.Println(now)
-	fmt.Println(now1)
+	fmt.Println("Current Time in CST Shnaghai : ", now)
+	fmt.Println("Current Time in IST Kolkata : ", now1)
 
 }
 
 func additon() {
 	// Get the current date
-	currentDate := time.Now()
+	todaysDate := time.Now()
 
 	// Add one year and two days to the current date
-	newDate := currentDate.AddDate(1, 0, 2)
+	newDate := todaysDate.AddDate(1, 0, 2)
 
 	// Format the new date as a string
-	formattedDate := newDate.Format("2006-01-02")
+	Date := newDate.Format("2006-01-02")
 
 	// Print the new date
-	fmt.Println("The date one year and two days after today is:", formattedDate)
+	fmt.Println("The date After one year and two days is:", Date)
 }
