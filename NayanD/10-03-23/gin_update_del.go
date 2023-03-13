8888f470f0cf39ff511d018f82a97785515c68f6
Nayandhoble <103194199+Nayandhoble@users.noreply.github.com>
2023-03-13 14:08:10 +0530

    Add files via upload

---
+++ NayanD/10-03-23/gin_update_del.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Book struct {
+	ID     int    `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+var books []Book
+
+func main() {
+	books = []Book{
+		{ID: 1, Title: "Atomic Habits", Author: "james clear"},
+		{ID: 2, Title: "Secret", Author: "Rhonda Byrne"},
+	}
+	r := gin.Default()
+
+	// Create a new book
+	r.POST("/add", func(c *gin.Context) {
+		var book Book
+		if err := c.ShouldBindJSON(&book); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		book.ID = len(books) + 1
+		books = append(books, book)
+
+		c.JSON(http.StatusCreated, book)
+	})
+
+	// Get all books
+	r.GET("/books", func(c *gin.Context) {
+		c.JSON(http.StatusOK, books)
+	})
+
+	// Get a single book
+	r.GET("/books/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		for _, book := range books {
+			if strconv.Itoa(book.ID) == id {
+				c.JSON(http.StatusOK, book)
+				return
+			}
+		}
+		c.JSON(http.StatusNotFound, gin.H{"message": "Book not found"})
+	})
+
+	// Update a book
+	r.PUT("/updatebooks/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		var updatedBook Book
+		if err := c.ShouldBindJSON(&updatedBook); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		for i, book := range books {
+			if strconv.Itoa(book.ID) == id {
+				updatedBook.ID = book.ID
+				books[i] = updatedBook
+				c.JSON(http.StatusOK, updatedBook)
+				return
+			}
+		}
+		c.JSON(http.StatusNotFound, gin.H{"message": "Book not found"})
+	})
+
+	// Delete a book
+	r.DELETE("/deletebooks/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		for i, book := range books {
+			if strconv.Itoa(book.ID) == id {
+				books = append(books[:i], books[i+1:]...)
+				c.JSON(http.StatusOK, gin.H{"message": "Book deleted"})
+				return
+			}
+		}
+		c.JSON(http.StatusNotFound, gin.H{"message": "Book not found"})
+	})
+
+	r.Run(":8080")
+}
